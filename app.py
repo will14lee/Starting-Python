298059d4b9374089d5562ce07284981c97c1e450
William <will14lee@gmail.com>
2022-04-18 03:22:03 -0400

    Add pop, index, count, sort, reverse, and copy functions

--- app.py
+++ app.py
@@ -84,19 +84,71 @@
 # print("But knowing my luck, I will only be able to " + verb + " " + adverb)
 # print("I really want to eat some " + breakfast_item + " while watching " + tv_show)
 
-sentence= ["let's", "eat", "Grandpa"]
-print(sentence[0] + " " + sentence[1] + " " + sentence[2])
-print(sentence[1] + " " + sentence[0] + " " + sentence[2])
-print(sentence[1] + " " + sentence[2] + " " + sentence[0])
-print(sentence[2] + " " + sentence[1] + " " + sentence[0])
-working_sentence= print(sentence[2] + " " + sentence[0] + " " + sentence[1])
+# sentence= ["let's", "eat", "Grandpa"]
+# print(sentence[0] + " " + sentence[1] + " " + sentence[2])
+# print(sentence[1] + " " + sentence[0] + " " + sentence[2])
+# print(sentence[1] + " " + sentence[2] + " " + sentence[0])
+# print(sentence[2] + " " + sentence[1] + " " + sentence[0])
+# working_sentence= print(sentence[2] + " " + sentence[0] + " " + sentence[1])
 
-sentence[2]= "Granpear"
-print(sentence[2] + " " + sentence[0] + " " + sentence[1])
+# sentence[2]= "Granpear"
+# print(sentence[2] + " " + sentence[0] + " " + sentence[1])
+
+from copy import copy
+
 
 other_sentence= ["Roast beef", "Fried Chicken", "a grape", "nothing"]
 
-sentence.append("Borscht")
-print(sentence[1:3])
-other_sentence.insert(1, "Lemons")
+# sentence.append("Borscht")
+# print(sentence[1:3])
+print("This is the initial list=")
 print(other_sentence)
+
+# # to put something before fried chicken
+# print("Adding Lemons to the list")
+# other_sentence.insert(1, "Lemons")
+# print(other_sentence)
+
+# print("Removing Lemons to the list")
+# other_sentence.remove("Lemons")
+# print(other_sentence)
+
+
+# print("This will destroy the entire list")
+# other_sentence.clear()
+# print("Well here's the list!")
+# print(other_sentence)
+
+
+
+
+
+
+
+
+
+
+# pop, index, count, count, sort, reverse, copy
+
+
+
+# other_sentence.pop()
+# print("After I pop something off it looks like this")
+# print(other_sentence)
+# print("\"nothing\" was removed from the list")
+
+# print(other_sentence.index("Fried Chicken"))
+
+# count the number of times that roast beef is in the list
+# other_sentence.insert(1, "Roast beef")
+# print(other_sentence.count("Roast beef"))
+
+
+# sorting the list
+# other_sentence.sort()
+
+# other_sentence.reverse()
+other_sentence2= other_sentence.copy()
+
+print("This is other_sentence2 list")
+print(other_sentence2)
